6084712b9906ac40e4da3795155b6eb8b3b1709d
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-07-04 16:22:43 +0900

    add img

--- src/app/Root.jsx
+++ src/app/Root.jsx
@@ -3,14 +3,25 @@ import PropTypes from 'prop-types';
 
 import TodoApp from './todo-app';
 
-const Root = ({ items }) => (
+const Root = ({
+  title,
+  items,
+}) => (
   <div>
-    <h1>Hello, SSR!</h1>
+    <h1>{title}</h1>
+
     <TodoApp items={items} />
+
+    <h3>Author</h3>
+    <a href="https://twitter.com/leader22">
+      <img src="./doseisan.jpg" width={150} height={150} />
+      <p>@leader22</p>
+    </a>
   </div>
 );
 
 Root.propTypes = {
+  title: PropTypes.string,
   items: PropTypes.arrayOf(PropTypes.object),
 };
 
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,18 +9,19 @@ const router = Router();
 
 router.get('/', (_, res) => {
   const state = {
+    title: 'Hello, SSR!',
     items: [
+      { text: 'These TODOs are created by server', id: 1499151555644 },
       { text: 'Styling', id: 1499151565644 },
       { text: 'Add README', id: 1499151667696 },
     ],
   };
-  const data = {
-    title: 'SSR mock',
+
+  res.send(render({
+    title: state.title,
     children: renderToString(<Root {...state} />),
-    // TODO: sanitize
     state: JSON.stringify(state),
-  };
-  res.send(render(data));
+  }));
 });
 
 export default router;
