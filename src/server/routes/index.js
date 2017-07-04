c016675b2d3aa44c6e8f5dcf714a733afe52c0ee
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-07-04 16:34:07 +0900

    fix routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,6 +8,16 @@ import Root from '../../app/root';
 const router = Router();
 
 router.get('/', (_, res) => {
+  const routes = [
+    { name: 'SSR', href: '/ssr', },
+    { name: 'initial data from server', href: '/data-only', },
+    { name: 'do nothing', href: '/nothing', },
+  ];
+  const el = routes.map(r => `<li><a href="${r.href}">${r.name}</a></li>`).join('');
+  res.send(`<ul>${el}</ul>`);
+});
+
+router.get('/ssr', (_, res) => {
   const state = {
     title: 'Hello, SSR!',
     items: [
@@ -21,6 +31,40 @@ router.get('/', (_, res) => {
     title: state.title,
     children: renderToString(<Root {...state} />),
     state: JSON.stringify(state),
+  }));
+});
+
+router.get('/data-only', (_, res) => {
+  const state = {
+    title: 'Hello, SSR!',
+    items: [
+      { text: 'These TODOs are created by server', id: 1499151555644 },
+      { text: 'Styling', id: 1499151565644 },
+      { text: 'Add README', id: 1499151667696 },
+    ],
+  };
+
+  res.send(render({
+    title: state.title,
+    children: '',
+    state: JSON.stringify(state),
+  }));
+});
+
+router.get('/nothing', (_, res) => {
+  const state = {
+    title: 'Hello, SSR!',
+    items: [
+      { text: 'These TODOs are created by server', id: 1499151555644 },
+      { text: 'Styling', id: 1499151565644 },
+      { text: 'Add README', id: 1499151667696 },
+    ],
+  };
+
+  res.send(render({
+    title: state.title,
+    children: '',
+    state: JSON.stringify({}),
   }));
 });
 
